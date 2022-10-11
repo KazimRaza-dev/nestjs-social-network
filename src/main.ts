e2bcda759219c49027b7cd7df1466af7c82ed423
kazim raza <kazim.raza@tintash.com>
2022-10-11 13:30:48 +0500

    what: procfile added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
